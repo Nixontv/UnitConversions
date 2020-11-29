9ec24d2e139107a604e794d99c44006540a47175
Nikita Velichko <velichkonikita@gmail.com>
2020-11-29 17:19:32 +0300

    Add empty project

---
+++ UnitConversions/UnitConversions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConversions
+//
+//  Created by Nikita Velichko on 11/29/20.
+//  Copyright © 2020 Nikita Velichko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
